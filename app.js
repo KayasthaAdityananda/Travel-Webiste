faec274e328c45b3f35fadcc66d51100069036a9
Aditya <adityanandakayastha@gmail.com>
2026-04-30 21:54:54 +0530

    Added error handler

--- app.js
+++ app.js
@@ -101,6 +101,9 @@ app.get("/listing/:id", async (req, res) =>{
     res.redirect("/listings");
 });
 
+app.use((req, res) => {
+    res.status(404).send("Page not found!");
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
